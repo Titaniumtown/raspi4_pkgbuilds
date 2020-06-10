d7871ec58dcc08713a18aad5c4301a376128aac9
Simon Gardling <titaniumtown@gmail.com>
2020-06-09 21:13:07 -0400

    do stuff

--- zfs-dkms/gpl_licenses.sh
+++ zfs-dkms/gpl_licenses.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 set -ex
-modify_zfs() {
-        cd /usr/src/$(ls ./usr/src | grep zfs | tail -n 1)
-        ./autogen.sh
-}
-(sed -i 's/CDDL/GPL/g' ./usr/src/zfs-*/META && modify_zfs) || true
+(sed -i 's/CDDL/GPL/g' ./usr/src/zfs-*/META) || true
 exit 0
